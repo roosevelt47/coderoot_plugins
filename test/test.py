e9efe9fe4af941867d669a69b199b68c398ed313
Jasraj Johal <44610634+roosevelt47@users.noreply.github.com>
2021-02-14 18:40:01 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,10 +10,8 @@ class social(commands.Cog):
     async def socials(self, ctx):
         embed = discord.Embed(
             title="**R&D Server**\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-            description="**Ranking System: **",
-            color=0xee3463
-        )
-        embed.add_field(name='', value="""• We are following the ranking system of our affiliated server, which is R&D Giveaways. Just chat in R&D Giveaways server to LEVEL UP.
+            description="""**Ranking System: **\n
+            • We are following the ranking system of our affiliated server, which is R&D Giveaways. Just chat in R&D Giveaways server to LEVEL UP.
             If you are level 3 or 10 on that server, please send a DM to @ DM to ask for help!
 
             Ranks with Benefits
@@ -21,7 +19,13 @@ class social(commands.Cog):
             <@&799230600039890954>: Users can gain access to Spotify Premium
             <@&799245176328552448>: Users can gain access to Netflix, HBO MAX, Disney +
 
-            Future roles and platforms will be distributed. Stay tuned to know more about upcoming updates!""",inline=False)
+            Future roles and platforms will be distributed. Stay tuned to know more about upcoming updates!
+            
+            
+            
+            """,
+            color=0xee3463
+        )
         embed.set_footer(text="Management Team",
                          icon_url="https://cdn.discordapp.com/icons/768497538839871519/a_48fd1abc638cf92fad8a8fb09c1ee04f.gif?size=1024")
         await ctx.send(embed=embed)
@@ -30,3 +34,4 @@ class social(commands.Cog):
 
 def setup(bot):
     bot.add_cog(social(bot))
+
