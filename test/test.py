cfccffe7e963c405ad9f027fc66a6409d0fa5451
Jasraj Johal <44610634+roosevelt47@users.noreply.github.com>
2021-02-14 18:21:59 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,12 +9,14 @@ class social(commands.Cog):
     async def socials(self, ctx):
         embed = discord.Embed(
             title="***R&D***\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-            description="**Discord:**\n• <https://discord.com/invite/FvmgmyDDRn>\n\n",
+            '''description="**Discord:**\n• <https://discord.com/invite/FvmgmyDDRn>\n\n",'''
             color=0xee3463,
             timestamp=ctx.message.created_at
         )
         embed.set_footer(text="Management Team", icon_url="https://cdn.discordapp.com/icons/768497538839871519/a_48fd1abc638cf92fad8a8fb09c1ee04f.gif?size=1024")
         await ctx.send(embed=embed)
+        Invite = f'discord.gg/FvmgmyDDRn`
+        await ctx.send(Invite)
         
         
 def setup(bot):
