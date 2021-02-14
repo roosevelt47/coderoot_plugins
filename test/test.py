b2c0f5ef82b65325225bd5b6d66bf25a26f5079d
Jasraj Johal <44610634+roosevelt47@users.noreply.github.com>
2021-02-14 18:24:14 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,15 +8,14 @@ class social(commands.Cog):
     @commands.command()
     async def socials(self, ctx):
         embed = discord.Embed(
-            title="***R&D***\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-            '''description="**Discord:**\n• <https://discord.com/invite/FvmgmyDDRn>\n\n",'''
+            title="**R&D Server**\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+            description="**Discord:**\n• to be removed",
             color=0xee3463,
             timestamp=ctx.message.created_at
         )
         embed.set_footer(text="Management Team", icon_url="https://cdn.discordapp.com/icons/768497538839871519/a_48fd1abc638cf92fad8a8fb09c1ee04f.gif?size=1024")
         await ctx.send(embed=embed)
-        Invite = f'discord.gg/FvmgmyDDRn`
-        await ctx.send(Invite)
+        await ctx.send("discord.gg/FvmgmyDDRn")
         
         
 def setup(bot):
